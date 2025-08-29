244d4bae8f35100a27136fcb347177988e6680f5
Edwin Portillo <weaselssh@gmail.com>
2025-08-29 09:31:49 -0600

    rm y mfile

--- said_edwinCMD/src/said_edwincmd/ManejoFiles.java
+++ said_edwinCMD/src/said_edwincmd/ManejoFiles.java
@@ -1,6 +1,7 @@
 package said_edwincmd;
 
 import java.io.File;
+import java.io.IOException;
 
 public class ManejoFiles {
 
@@ -24,10 +25,54 @@ public class ManejoFiles {
         }
 
         if (target.mkdirs()) {
-            System.out.println("Carpeta creada exitosamente: " + target.getPath());
+            System.out.println("Directorio creado exitosamente: " + target.getPath());
             return true;
         } else {
-            System.out.println("Error: no se pudo crear la carpeta.");
+            System.out.println("Error: no se pudo crear el directorio.");
+            return false;
+        }
+    }
+
+    public boolean mfile(String nombre) throws IOException {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            System.out.println("Error: nombre no válido.");
+            return false;
+        }
+
+        File target = new File(rutaActual, nombre);
+
+        if (target.exists()) {
+            System.out.println("Error: un archivo/directorio ya existe " + target.getPath());
+            return false;
+        }
+
+        if (target.createNewFile()) {
+            System.out.println("Archivo creado exitosamente: " + target.getPath());
+            return true;
+        } else {
+            System.out.println("Error: no se pudo crear el archivo.");
+            return false;
+        }
+    }
+
+    public boolean rm(String nombre) {
+        if (nombre == null || nombre.trim().isEmpty()) {
+            System.out.println("Error: nombre no válido.");
+            return false;
+        }
+
+        File target = new File(rutaActual, nombre);
+
+        if (!target.exists()) {
+            System.out.println("Error: el archivo/folder no existe " + target.getPath());
+            return false;
+        }
+
+        if (target.delete()) {
+            System.out.println("Archivo/directorio eliminado exitosamente: " + target.getPath());
+            return true;
+        } else {
+            System.out.println("Error: no se pudo eliminar el archivo/directorio.");
             return false;
         }
     }
